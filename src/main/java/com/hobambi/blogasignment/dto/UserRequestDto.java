e20cab9dec8bc1dc9bf95a852f1a77dc6b4788c8
hobambi <yyunsunny@gmail.com>
2023-02-14 11:55:34 +0900

    회원가입 끝

--- src/main/java/com/hobambi/blogasignment/dto/UserRequestDto.java
+++ src/main/java/com/hobambi/blogasignment/dto/UserRequestDto.java
@@ -3,9 +3,21 @@ package com.hobambi.blogasignment.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 @Setter
 @Getter
 public class UserRequestDto {
+
+    @Size(min=4,max=10)
+    @Pattern(regexp = "^[a-z0-9]*$")
+    @NotNull(message = "이름을 입력해주세요.")
     private String username;
+
+    @Size(min=8,max=15)
+    @Pattern(regexp = "^[A-Za-z0-9]*$")
+    @NotNull(message = "비밀번호를 입력해주세요.")
     private String password;
 }
--- src/main/java/com/hobambi/blogasignment/service/UserService.java
+++ src/main/java/com/hobambi/blogasignment/service/UserService.java
@@ -8,17 +8,20 @@ import com.hobambi.blogasignment.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.Valid;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Validated
 public class UserService {
     private final UserRepository userRepository;
 
 
     @Transactional
-    public UserResponseDto signup(UserRequestDto userRequestDto) {
+    public UserResponseDto signup(@Valid UserRequestDto userRequestDto) {
         String username = userRequestDto.getUsername();
 
        Optional<User> found = userRepository.findByUsername(username);
