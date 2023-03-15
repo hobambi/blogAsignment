f1fb7d0f3594340c92364a765db93de3c3ffada1
hobambi <yyunsunny@gmail.com>
2023-03-15 19:52:49 +0900

    폴더 이동 테스트

---
+++ src/main/java/com/hobambi/blogasignment/Controller/LikeController.java
@@ -0,0 +1,14 @@
+package com.hobambi.blogasignment.Controller;
+
+import com.hobambi.blogasignment.exceptionTest.ApiResult;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class LikeController {
+
+//    @PostMapping("/api/like")
+//    public ApiResult<>like
+}
